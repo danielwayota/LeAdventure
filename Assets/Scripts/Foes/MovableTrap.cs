e23cfbde7477a54acbe7897ecf4b2d8e6c5355cd
Daniel Wayota <danielglez1990@gmail.com>
2018-09-19 15:08:28 +0100

    Movable traps

---
+++ Assets/Scripts/Foes/MovableTrap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MovableTrap : Trap
+{
+	public Vector2 offset;
+	public float moveSpeed = 1;
+
+	private Vector3 startPosition;
+	private Vector3 currentOffset;
+
+	private float theta;
+
+	// ============================
+	void Start()
+	{
+		this.startPosition = this.transform.position;
+		this.theta = 0;
+	}
+
+	// ============================
+    void Update()
+    {
+		this.theta += Time.deltaTime * this.moveSpeed;
+
+		this.currentOffset.Set(
+			this.offset.x * Mathf.Cos(theta),
+			this.offset.y * Mathf.Sin(theta),
+			0
+		);
+
+		this.transform.position = this.startPosition + this.currentOffset;
+    }
+}
