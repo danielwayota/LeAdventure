af0fccfaa3e05f0a6fdf7e3cd142afc7bb86cde1
Daniel Wayota <danielglez1990@gmail.com>
2018-09-28 11:30:19 +0100

    Movable Enemy

---
+++ Assets/Scripts/Foes/MovableEnemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MovableEnemy : MovableTrap
+{
+	protected override void PlayerCollision(Hero h)
+	{
+		if (h.UseSword())
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			h.Die();
+		}
+	}
+}
