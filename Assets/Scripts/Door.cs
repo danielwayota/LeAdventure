526a6cb3add40b6ae3a5ad9ed51d290216c9bae7
Daniel Wayota <danielglez1990@gmail.com>
2018-09-30 13:19:10 +0100

    Door with changing sprite and some prefrabs

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,13 +2,35 @@
 
 public class Door : MonoBehaviour
 {
+    public Sprite closed;
+    public Sprite opened;
+
+    private SpriteRenderer leRenderer;
+
+    private BoxCollider2D[] colliders;
+
+    // ================================
+    void Start()
+    {
+        this.leRenderer = GetComponent<SpriteRenderer>();
+
+        this.leRenderer.sprite = closed;
+
+        this.colliders = GetComponents<BoxCollider2D>();
+    }
+
+    // ================================
     private void OnTriggerEnter2D(Collider2D other)
     {
         Hero h = other.gameObject.GetComponent<Hero>();
 
         if (h.UseKey())
         {
-            Destroy(this.gameObject);
+            this.leRenderer.sprite = opened;
+            foreach (BoxCollider2D col in this.colliders)
+            {
+                col.enabled = false;
+            }
         }
     }
 }
